f7f06713e6846f5a167d4b77b7917d85e050d4ba
Franjmg <fran@fi.unju.edu.ar>
2024-06-23 19:12:00 -0300

    New functions were created to know the name of the product and be able to use it in the product alert.

--- backend/src/alerts/alerts.py
+++ backend/src/alerts/alerts.py
@@ -5,10 +5,10 @@ from email.mime.text import MIMEText
 from decouple import config
 
 
-def comparar_productos():
+def comparar_productos(producto):
     ruta_archivo = 'backend/productos.json'
     productos_previos = scraper.leer_productos_json(ruta_archivo)
-    productos_actuales = scraper.get_productos()
+    productos_actuales = scraper.get_productos_final(producto)
 
     # Convertir la lista previa en un diccionario para facilitar la comparación
     dicc_productos_previos = {p['titulo']: p for p in productos_previos}
@@ -17,24 +17,24 @@ def comparar_productos():
         titulo = producto['titulo']
         if titulo in dicc_productos_previos:
             precio_previo = dicc_productos_previos[titulo]['precio']
+            imagen_previa = dicc_productos_previos[titulo]['imagen']
             if producto['precio'] != precio_previo:
                 return(f"El precio de '{titulo}' cambió de {precio_previo} a {producto['precio']}")
-        else:
-            return(f"Nuevo producto encontrado: {titulo}")
-
+            elif producto['imagen'] != imagen_previa:
+                return(f"La imagen de '{titulo}' cambió {imagen_previa} a {producto['imagen']}")
 
 
 def enviar_cambios_email(cambios):
     # Configuración del servidor SMTP y credenciales
     servidor_smtp = "smtp.gmail.com"
     puerto_smtp = 587
-    correo_remitente = config('EMAIL')
+    correo_remitente = config('EMAIL_HOST')
     contraseña_remitente = config('PASSWORD')
     
     # Crear el mensaje de correo electrónico
     mensaje = MIMEMultipart()
     mensaje['From'] = correo_remitente
-    mensaje['To'] = "42227624@fi.unju.edu.ar"
+    mensaje['To'] = config('EMAIL_DEST')
     mensaje['Subject'] = "Cambios de Precios"
     
     # El cuerpo del correo con los cambios
--- backend/src/scraper/scraper.py
+++ backend/src/scraper/scraper.py
@@ -4,15 +4,19 @@ from bs4 import BeautifulSoup
 import json
 
 
-url = "https://listado.mercadolibre.com.ar/notebooks#D[A:notebooks]"
-response = requests.get(url)
+def url_dinamica(producto):
+    url = f"https://listado.mercadolibre.com.ar/{producto}#D[A:{producto}]"
+    response = requests.get(url)
+    return response
 
-def get_productos():
+
+def get_productos(producto, itemclass):
+    response = url_dinamica(producto)
     productos_list = []  # Lista para almacenar datos de productos
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        productos = soup.find_all('li', class_="ui-search-layout__item shops__layout-item ui-search-layout__stack")
+        productos = soup.find_all('li', class_=itemclass)
         
         for i, producto in enumerate(productos):
             if i < 15:  # Limita a los primeros 15 productos
@@ -29,6 +33,15 @@ def get_productos():
         print("Error al cargar la web, codigo", response.status_code)
     return productos_list
 
+def get_productos_final(producto):
+    itemclass =  "ui-search-layout__item shops__layout-item ui-search-layout__stack"
+    lista = get_productos(producto, itemclass)
+    if not lista: 
+        lista = get_productos(producto, "ui-search-layout__item")
+        return lista
+    else:
+        return lista
+
 def leer_productos_json(ruta_archivo):
     try:
         with open(ruta_archivo, 'r', encoding='utf-8') as f:
@@ -39,3 +52,17 @@ def leer_productos_json(ruta_archivo):
 def guardar_productos_json(productos, ruta_archivo):
     with open(ruta_archivo, 'w', encoding='utf-8') as f:
         json.dump(productos, f, ensure_ascii=False, indent=4)
+
+def guardar_nombre_producto(producto, ruta_archivo):
+    with open(ruta_archivo, 'w', encoding='utf-8') as f:
+        json.dump([{"nombre_producto": producto}], f, ensure_ascii=False)
+
+def obtener_nombre_producto(ruta_archivo):
+    with open(ruta_archivo, 'r') as archivo:
+        productos = json.load(archivo)
+    
+    # Asumiendo que quieres la primera palabra del nombre del primer producto
+    primer_producto = productos[0]  # Accede al primer producto
+    nombre_primer_producto = primer_producto['nombre_producto']  # Accede al nombre
+    primera_palabra = nombre_primer_producto.split()[0]  # Extrae la primera palabra
+    return primera_palabra
