51a6511a84226ae249141a6035322c6eb0f94f2a
Franjmg <fran@fi.unju.edu.ar>
2024-06-23 19:13:09 -0300

    The main was modified to know the name of the stored product

--- backend/app.py
+++ backend/app.py
@@ -4,17 +4,32 @@ import src.alerts.alerts as alerts
 
 def menu():
     while True:
+
+        print("\nMenú:")
+        producto = scraper.obtener_nombre_producto("backend/nameproducto.json")
+        print(f"El producto almacenado es {producto}")
+        print("0. Actualizar productos")
         print("1. Ver productos")
         print("2. Comparar precios")
         print("3. Salir")
         opcion = input("Ingrese una opción: ")
 
-        if opcion == "1":
-            scraper.guardar_productos_json(scraper.get_productos(), "backend/productos.json")
+        
+        
+
+        if opcion == "0":
+            print("Ingrese nuevo producto:")
+            productonuevo = input()
+            scraper.guardar_nombre_producto(productonuevo, "backend/nameproducto.json")
+            scraper.guardar_productos_json(scraper.get_productos_final(productonuevo), "backend/productos.json")
+            print(scraper.leer_productos_json("backend/productos.json"))
+            print("Productos actualizados.")
+
+        elif opcion == "1":
             print(scraper.leer_productos_json("backend/productos.json"))
 
         elif opcion == "2":
-            cambios = alerts.comparar_productos()
+            cambios = alerts.comparar_productos(producto)
             print(cambios)
             if cambios:
                 alerts.enviar_cambios_email(cambios)
